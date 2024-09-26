67d008745e66d715822ba5c1eec596b034e1b7af
Nicolas <nicolasfigueiredo381@gmail.com>
2024-09-26 15:42:44 -0300

    Salvando

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            //Exercício1.Atividade1();
-            //Exercício2.Atividade2();
-            //Exercício3.Atividade3();
-            //Exercício4.Atividade4();
+            Exercício1.Atividade1();
+            Exercício2.Atividade2();
+            Exercício3.Atividade3();
+            Exercício4.Atividade4();
             Exercício5.Atividade5();
         }
     }
